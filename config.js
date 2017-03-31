96717d96d964cbe2827accfcdc042abda7a01b1a
mengxcSemioe <matt.meng@semioe.com>
2017-03-31 11:51:08 +0800

    fix bug

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://ghost.mengxc.info',
+        url: 'http://blog.mengxc.info',
         mail: {},
         database: {
             client: 'sqlite3',
